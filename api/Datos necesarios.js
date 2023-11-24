8c60d7bdff3551be9243d87361b3eeba4a9e1101
genac4 <genacura4@gmail.com>
2023-11-24 17:01:10 -0300

    Todo ok

---
+++ api/Datos necesarios.js
@@ -0,0 +1,5 @@
+//Estos datos tenes que agregarlos a un archivo .env y colocar tus credenciales.
+// DB_USER=postgres
+// DB_PASSWORD= Aca van tu password de SQL
+// DB_HOST=localhost
+// API_KEY= ACA VA UNA API KEY
--- api/archivoparaguardarmisdatos.js
+++
@@ -1,5 +0,0 @@
-//Luego de terminar el pi tengo que borrarlos.
-// DB_USER=postgres
-// DB_PASSWORD=Gena123 
-// DB_HOST=localhost
-// API_KEY=0552c678a4994b7baf3c05fc2810b6f0
